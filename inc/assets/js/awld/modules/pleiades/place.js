aa837e9d28d4f62a5f086684ea9409eea904039c
donutz <donutz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-19 20:21:52 +0000

    update trunk to 0.2.0
    git-svn-id: https://plugins.svn.wordpress.org/ancient-world-linked-data-for-wordpress/trunk@1010123 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/assets/js/awld/modules/pleiades/place.js
+++ inc/assets/js/awld/modules/pleiades/place.js
@@ -12,7 +12,7 @@ define(function() {
         parseData: function(data) {
             data.name = data.title;
             data.latlon = data.reprPoint && data.reprPoint.reverse();
-            data.description = data.description + " <br/><a href='http://pelagios.dme.ait.ac.at/api/places/http%3A%2F%2Fpleiades.stoa.org%2Fplaces%2F"+data.id+"'>Further references at Pelagios</a>";
+            data.description = 'A place described in the Pleiades gazetteer: ' + data.description + " <br/><a href='http://pelagios.dme.ait.ac.at/api/places/http%3A%2F%2Fpleiades.stoa.org%2Fplaces%2F"+data.id+"'>Further information at Pelagios</a>";
             return data;
         }
     };
